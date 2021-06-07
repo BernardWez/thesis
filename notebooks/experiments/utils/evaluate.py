1262ccbdde51d241c56afc8049bb902a7da5c455
BernardWez <bfwezeman@gmail.com>
2021-06-07 02:04:51 +0200

    Update evaluation process

--- notebooks/experiments/utils/evaluate.py
+++ notebooks/experiments/utils/evaluate.py
@@ -1,4 +1,5 @@
-import copy
+import os
+import pandas as pd
 
 
 def calc_precision(tp, fp):
@@ -13,17 +14,60 @@ def calc_fscore(precision, recall):
     return 2 * (precision * recall) / (precision + recall)
 
 
-def evaluate(gold_truth_labels, predictions):
+def calc_accuracy(tp, total_annotations):
+    return tp / total_annotations
+
+
+def evaluate(gold_truth_labels, predictions, model_name, dataset, filtered):
+
+    fps_strict, fns_strict, tps_strict = run_evaluation(
+        gold_truth_labels, predictions, 'strict', model_name, dataset, filtered)
+
+    fps_forgiving, fns_forgiving, tps_forgiving = run_evaluation(
+        gold_truth_labels, predictions, 'forgiving', model_name, dataset, filtered)
+
+    return ((fps_strict, fns_strict, tps_strict), (fps_forgiving, fns_forgiving, tps_forgiving))
+
+
+def store_results(model_name, dataset, mode, precision,
+                  recall, f_score, accuracy, filtered):
+
+    # Load results dataframe
+    results = pd.read_pickle('../../../results/results.pkl')
+
+    # Add results to dataframe
+    results = results.append({'model': model_name,
+                              'dataset': dataset,
+                              'mode': mode,
+                              'filtered': filtered,
+                              'precision': precision,
+                              'recall': recall,
+                              'f1': f_score,
+                              'acc': accuracy}, ignore_index=True)
+
+    # Remove duplicates
+    results = results.drop_duplicates(
+        ['model', 'dataset', 'mode', 'filtered'], keep='last')
+
+    # Write back to disk
+    results.to_pickle("../../../results/results.pkl")
+
+
+def run_evaluation(gold_truth_labels, predictions, mode, model_name, dataset, filtered):
     # Counts of true positives, false positives & false negatives
     tp, fp, fn = 0, 0, 0
 
-    # List with false positives and false negatives
-    fps, fns = [], []
+    # List with false positives, false negatives, and true positives
+    fps, fns, tps = [], [], []
 
     for gold, pred in zip(gold_truth_labels, predictions):
 
-        tp_tmp, fp_tmp, fn_tmp, fns_temp, fps_temp = evaluate_one_article(
-            gold, pred)
+        if mode == 'strict':
+            tp_tmp, fp_tmp, fn_tmp, fns_temp, fps_temp, tps_temp = evaluate_one_article_strict(
+                gold, pred)
+        elif mode == 'forgiving':
+            tp_tmp, fp_tmp, fn_tmp, fns_temp, fps_temp, tps_temp = evaluate_one_article_forgiving(
+                gold, pred)
 
         tp += tp_tmp
         fp += fp_tmp
@@ -31,19 +75,30 @@ def evaluate(gold_truth_labels, predictions):
 
         fns.extend(fns_temp)
         fps.extend(fps_temp)
+        tps.extend(tps_temp)
 
     precision = calc_precision(tp, fp)
     recall = calc_recall(tp, fn)
     f_score = calc_fscore(precision, recall)
 
+    total_annotations = sum([len(entry['entities'])
+                            for entry in gold_truth_labels])
+    accuracy = calc_accuracy(tp, total_annotations)
+
+    store_results(model_name, dataset, mode, precision,
+                  recall, f_score, accuracy, filtered)
+
+    print(f'Evaluation mode: {mode}')
     print(f'fp: {fp} | tp: {tp} | fn: {fn}')
     print(
-        f'precision: {precision:.3f} | recall: {recall:.3f} | f-score: {f_score:.3f}')
+        f'precision: {precision:.3f} | recall: {recall:.3f} | f-score: {f_score:.3f} | accuracy: {accuracy:.3f}')
+    print('------------------------------------------------------------------------')
+    print()
 
-    return fps, fns
+    return fps, fns, tps
 
 
-def evaluate_one_article(gold_truth, prediction):
+def evaluate_one_article_strict(gold_truth, prediction):
 
     gold = gold_truth['entities'].copy()
     pred = prediction['entities'].copy()
@@ -52,16 +107,15 @@ def evaluate_one_article(gold_truth, prediction):
     tp, fp, fn = 0, 0, 0
 
     # List with false positives and false negatives
-    fps, fns = [], []
-
-    i = 0
+    fps, fns, tps = [], [], []
 
     while len(gold) > 0 and len(pred) > 0:
-        i += 1
 
         # Check if the first two elements are the same
         if gold[0] == pred[0]:
             tp += 1
+            tps.append(pred[0]['text'])
+
             gold.pop(0)
             pred.pop(0)
 
@@ -85,4 +139,75 @@ def evaluate_one_article(gold_truth, prediction):
     elif len(pred) > 0:
         fp += 1
 
-    return tp, fp, fn, fns, fps
+    return tp, fp, fn, fns, fps, tps
+
+
+def evaluate_one_article_forgiving(gold_truth, prediction):
+
+    gold = gold_truth['entities'].copy()
+    pred = prediction['entities'].copy()
+
+    # Counts of true positives, false positives & false negatives
+    tp, fp, fn = 0, 0, 0
+
+    # List with false positives and false negatives
+    fps, fns, tps = [], [], []
+
+    while gold and pred:
+
+        # Check if the first two elements have overlap
+        if (
+            # case 1: prediction starts before gold annotation and ends after gold start
+            (pred[0]['start_pos'] < gold[0]['start_pos']
+             and pred[0]['end_pos'] > gold[0]['start_pos'])
+
+            or
+
+            # case 2: prediction is within gold annotation boundaries
+            (pred[0]['start_pos'] >= gold[0]['start_pos']
+             and pred[0]['end_pos'] <= gold[0]['start_pos'])
+
+            or
+
+            # case 3: prediction starts before gold end and ends at/after gold end
+            (pred[0]['start_pos'] < gold[0]['end_pos']
+             and pred[0]['end_pos'] >= gold[0]['end_pos'])
+
+        ):
+
+            tp += 1
+            tps.append(pred[0]['text'])
+            pred.pop(0)
+
+            # Break if last predictions has been handeled
+            if not pred:
+                gold.pop(0)
+                break
+            # elif 'next prediction' starts before gold annotation ends: skip to next iteration
+            elif pred[0]['start_pos'] < gold[0]['end_pos']:
+                continue
+            # Else 'next prediction' starts after: remove gold annotation
+            else:
+                gold.pop(0)
+
+        else:
+            # Grab the first appearing element
+            element, source = (
+                gold[0], 'gold') if gold[0]['start_pos'] < pred[0]['start_pos'] else (pred[0], 'pred')
+
+            # Remove the element first appearing element
+            if source == 'gold':
+                fn += 1
+                fns.append(element['text'])
+                gold.remove(element)
+            elif source == 'pred':
+                fp += 1
+                fps.append(element['text'])
+                pred.remove(element)
+
+    if gold:
+        fn += 1
+    elif pred:
+        fp += 1
+
+    return tp, fp, fn, fns, fps, tps
