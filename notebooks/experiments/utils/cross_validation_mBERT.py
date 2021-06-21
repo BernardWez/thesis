489fb885abc7acf90d6fa767293f4710deb561f4
BernardWez <bfwezeman@gmail.com>
2021-06-21 23:09:21 +0200

    Run mBERT cross validation

--- notebooks/experiments/utils/cross_validation_mBERT.py
+++ notebooks/experiments/utils/cross_validation_mBERT.py
@@ -37,4 +37,86 @@ def cross_validation_mBERT(model, tokenizer, label_list, fold):
     evaluate(data_DPD, processed_results, model_name='mBERT',
              dataset=dataset, filtered=filtered, cv=fold)
 
-    print('Yay')
+    # TR-News
+    file_path = '../../../data/TR-News/TR-News.xml'
+    filtered = False
+    dataset = 'TR-News'
+
+    # Load data
+    data_TRN = prepare_data(file_path, filtered=filtered, split=True)
+
+    # Process data
+    TRN = prepare_dataset(data_TRN, tokenizer)
+
+    # Evaluation trainer
+    data_collator = DataCollatorForTokenClassification(tokenizer)
+
+    eval_trainer = Trainer(model,
+                           data_collator=data_collator)
+
+    # Predictions
+    raw_pred, _, _ = eval_trainer.predict(TRN)
+    predictions = np.argmax(raw_pred, axis=2)
+
+    processed_results = process_predictions(
+        predictions, TRN, label_list, tokenizer)
+
+    # Evaluate
+    evaluate(data_TRN, processed_results, model_name='mBERT',
+             dataset=dataset, filtered=filtered, cv=fold)
+
+    # LGL
+    file_path = '../../../data/LGL/LGL.xml'
+    filtered = False
+    dataset = 'LGL'
+
+    # Load data
+    data_LGL = prepare_data(file_path, filtered=filtered, split=True)
+
+    # Process data
+    LGL = prepare_dataset(data_LGL, tokenizer)
+
+    # Evaluation trainer
+    data_collator = DataCollatorForTokenClassification(tokenizer)
+
+    eval_trainer = Trainer(model,
+                           data_collator=data_collator)
+
+    # Predictions
+    raw_pred, _, _ = eval_trainer.predict(LGL)
+    predictions = np.argmax(raw_pred, axis=2)
+
+    processed_results = process_predictions(
+        predictions, LGL, label_list, tokenizer)
+
+    # Evaluate
+    evaluate(data_LGL, processed_results, model_name='mBERT',
+             dataset=dataset, filtered=filtered, cv=fold)
+
+    # GeoWebNews
+    file_path = '../../../data/GeoWebNews/GeoWebNews.xml'
+    filtered = True
+    dataset = 'GWN'
+
+    # Load data
+    data_GWN = prepare_data(file_path, filtered=filtered, split=True)
+
+    # Process data
+    GWN = prepare_dataset(data_GWN, tokenizer)
+
+    # Evaluation trainer
+    data_collator = DataCollatorForTokenClassification(tokenizer)
+
+    eval_trainer = Trainer(model,
+                           data_collator=data_collator)
+
+    # Predictions
+    raw_pred, _, _ = eval_trainer.predict(GWN)
+    predictions = np.argmax(raw_pred, axis=2)
+
+    processed_results = process_predictions(
+        predictions, GWN, label_list, tokenizer)
+
+    # Evaluate
+    evaluate(data_GWN, processed_results, model_name='mBERT',
+             dataset=dataset, filtered=filtered, cv=fold)
